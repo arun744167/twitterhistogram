5b26dcf707333c7973c9823a14606bafe8ade7c9
arun744-newscorp <57883793+arun744-newscorp@users.noreply.github.com>
2021-12-07 20:10:53 +1100

    factor api code and add read me

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -11,46 +11,95 @@ use File;
 
 class TwitterController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     *
+     * index function call which sends data to view template
+     */
     public function twitterSearch( Request $request ){
 
         $tweetHourPollList = [];
+        $userId = $request->session()->get( 'twitterUserId' );
 
-        try {
-            $params = [ 'place.fields' => 'country,name',
-                        'tweet.fields' => 'created_at',
-                        'max_results' => 100,
-                        'expansions' => 'author_id,in_reply_to_user_id',
-                        'response_format' => 'json',
-            ];
+        if ( !empty( $userId ) ) {
 
-            $userId = $request->session()->get( 'twitterUserId' );
+            try {
+                $allTweets = $this->getAllTweets( $userId );
+                foreach ( $allTweets as $value ){
+                    $convertData = json_decode( $value );
 
-            if ( !empty( $userId ) ) {
-                $data = Twitter::userTweets( $userId, $params );
-                $decodedData = json_decode( $data );
-
-                foreach( $decodedData->data as $tweetRecord ) {
-                    $date = strtotime($tweetRecord->created_at);
-                    array_push($tweetHourPollList, date('H', $date) );
+                    foreach ( $convertData->data as $tweetRecord ){
+                        $date = strtotime($tweetRecord->created_at);
+                        array_push( $tweetHourPollList, date('H', $date) );
+                    }
                 }
+            } catch(  \Exception $e ){
+                return view('twittergraph', [ 'errorMessage' => "Invalid username entered" ] );
             }
-
-        } catch( \Exception $e)  {
-            $request->session()->forget('twitterUserId');
-            return view('twittergraph', [ 'errorMessage' => $e->getMessage() ] );
         }
 
         $chartData = array_count_values( $tweetHourPollList );
         ksort($chartData );
+
+        if ( ! empty ( $chartData ) ) {
+            $twitterChart = $this->buildGraph($chartData );
+            return view('twittergraph', [ 'twitterGraph' => $twitterChart ] );
+        }
+    }
+
+
+    /**
+     * @param $chartData
+     * @return TwitterChart
+     *
+     * build histogram based on chartdata value.
+     */
+    private function buildGraph ( $chartData ) {
         $twitterChart = new TwitterChart();
 
-        if ( ! empty ( $chartData) ){
-            $twitterChart->labels( array_keys( $chartData) );
-            $twitterChart->dataset('x axis - 24 hrs , y axis - tweet counts', 'bar', array_values( $chartData ) )
+        $twitterChart->labels( array_keys( $chartData) );
+        $twitterChart->dataset('x axis - 24 hrs , y axis - tweet counts', 'bar', array_values( $chartData ) )
                 ->color("rgb(123, 104, 238)")
                 ->backgroundcolor("rgb(123, 104, 238)");
-        }
 
-        return view('twittergraph', [ 'twitterGraph' => $twitterChart ] );
+        return $twitterChart;
+    }
+
+    /***
+     * @param $userid
+     * @return array
+     *
+     * iterate 5 times to get 500 tweets
+     */
+    private function getAllTweets( $userid ) {
+
+        $tweetList = [];
+
+        for ( $index = 0; $index < 5; $index ++) {
+
+            $token = null;
+            if (!empty( $data ) ){
+                $decodedData = json_decode( $data );
+                $token = $decodedData->meta->next_token;
+            }
+
+
+            $params = [
+                'place.fields' => 'country,name',
+                'tweet.fields' => 'created_at',
+                'max_results' => 100,
+                'expansions' => 'author_id,in_reply_to_user_id',
+                'pagination_token' => $token,
+                'next_token' => [],
+                'response_format' => 'json',
+            ];
+
+            $data = Twitter::userTweets( $userid, $params );
+
+            array_push( $tweetList, $data);
+         }
+
+        return $tweetList;
     }
 }
